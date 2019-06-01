4318acf548ceca41d42d7efa89315f210c0f07de
GloriaKitchens <gloria.kitchens@tufts.edu>
2019-06-01 18:48:53 -0400

    removed empty line

--- src/main/java/com/google/codeu/servlets/UserListServlet.java
+++ src/main/java/com/google/codeu/servlets/UserListServlet.java
@@ -1,7 +1,6 @@
 package com.google.codeu.servlets;
 
 import java.io.IOException;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,4 +18,16 @@ public class UserListServlet extends HttpServlet {
 
     response.getOutputStream().println("this will be my user list");
   }
+
+  public Set<String> getUsers(){
+      Set<String> users = new HashSet<>();
+      Query query = new Query("Message");
+      PreparedQuery results = datastore.prepare(query);
+      for(Entity entity : results.asIterable()) {
+          users.add((String) entity.getProperty("user"));
+      }
+      return users;
+  }
+
+ // System.out.println(getUsers());
 }
