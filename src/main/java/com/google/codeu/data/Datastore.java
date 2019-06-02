3cb58edcf8350b1f30dbb3a40d5e534bbbee46e1
biancalyons <biancalyons007>
2019-06-02 15:30:05 -0400

    Added a closing '}' in the Datastore class, updated function name in MessageServlet class, the MessageFeedServlet now responds with a JSON representation of message data for all users when the user requests the /feed URL.

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -78,3 +78,4 @@ public class Datastore {
 
    return messages;
   }
+}
--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -58,7 +58,7 @@ public class MessageServlet extends HttpServlet {
       return;
     }
 
-    List<Message> messages = datastore.getMessages(user);
+    List<Message> messages = datastore.getAllMessages();
     Gson gson = new Gson();
     String json = gson.toJson(messages);
 
